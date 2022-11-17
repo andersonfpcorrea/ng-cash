2810884d857aaa937ba02125498fbab05dfa489b
Anderson Correa <andersonfpcorrea@gmail.com>
2022-11-17 18:30:43 -0300

    feat: complete Home page

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -2,18 +2,11 @@ import { ReactElement } from "react";
 import {
   Box,
   Flex,
-  Avatar,
   Button,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
+  Link,
   useColorModeValue,
   Stack,
   useColorMode,
-  Center,
-  Link,
 } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
@@ -22,50 +15,27 @@ export default function NavBar({ user }: { user: string }): ReactElement {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <>
-      <Box bg={useColorModeValue("blue.400", "gray.900")} px={4} w="full">
+      <Box bg={useColorModeValue("blue.400", "gray.900")} p={4} w="full">
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <Box className="font-bold text-xl">NG Cash</Box>
+          <Box className="font-bold text-xl">NG.CASH</Box>
 
           <Flex alignItems={"center"}>
             <Stack direction={"row"} spacing={7}>
               <Button onClick={toggleColorMode}>
                 {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
               </Button>
-
-              <Menu>
-                <MenuButton
-                  as={Button}
-                  rounded={"full"}
-                  variant={"link"}
-                  cursor={"pointer"}
-                  minW={0}
+              <Link as={RouterLink} to={"/"}>
+                <Button
+                  aria-label="logout"
+                  color={"white"}
+                  _hover={{
+                    bg: "red.400",
+                  }}
+                  bg={useColorModeValue("blue.400", "gray.900")}
                 >
-                  <Avatar
-                    size={"sm"}
-                    src={"https://avatars.dicebear.com/api/male/username.svg"}
-                  />
-                </MenuButton>
-                <MenuList alignItems={"center"}>
-                  <br />
-                  <Center>
-                    <Avatar
-                      size={"2xl"}
-                      src={"https://avatars.dicebear.com/api/male/username.svg"}
-                    />
-                  </Center>
-                  <br />
-                  <Center>
-                    <p>{user}</p>
-                  </Center>
-                  <br />
-                  <MenuDivider />
-                  <MenuItem>
-                    <Link as={RouterLink} to="/">
-                      Logout
-                    </Link>
-                  </MenuItem>
-                </MenuList>
-              </Menu>
+                  Log-out
+                </Button>
+              </Link>
             </Stack>
           </Flex>
         </Flex>
