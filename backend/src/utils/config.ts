e029b66ee146f0f5b7e474c8c8c8c78ee7f5ec27
Anderson Correa <andersonfpcorrea@gmail.com>
2022-11-18 18:48:48 -0300

    feat: initial server setup

---
+++ backend/src/utils/config.ts
@@ -0,0 +1,10 @@
+export const statusCodes = {
+  OK: 200,
+  NOT_FOUND: 404,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NO_CONTENT: 204,
+};
+
+export const PORT = process.env.PORT ?? 3001;
